36ae1e0b9666ebb2b03dd84216b062e100f07892
Lleyton Gray <lleyton@innatical.com>
2020-08-23 19:34:43 -0700

    Fix discriminator formatting

--- src/sidebar/Sidebar.tsx
+++ src/sidebar/Sidebar.tsx
@@ -65,7 +65,7 @@ export const Sidebar = () => {
       <div className={styles.profile}>
         <img src={user.data?.avatar} alt={user.data?.username} />
         <h4>
-          {user.data?.username}#{user.data?.discriminator}
+          {user.data?.username}#{user.data?.discriminator.toString().padStart(4, '0')}
         </h4>
         {/* <FontAwesomeIcon icon={faUserCog} fixedWidth/> */}
       </div>
