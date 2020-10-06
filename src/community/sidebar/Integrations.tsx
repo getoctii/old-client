ca4fff238fe9e13f1fb463629406f6b055168195
Adam <adam@atiktech.co>
2020-10-05 20:02:16 -0500

    📝 actually added the members tab

--- src/community/sidebar/Integrations.tsx
+++ src/community/sidebar/Integrations.tsx
@@ -1,4 +1,4 @@
-import { faUsersCog } from '@fortawesome/pro-solid-svg-icons'
+import { faUsersCog, faAddressBook } from '@fortawesome/pro-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React from 'react'
 import { useHistory, useRouteMatch } from 'react-router-dom'
@@ -18,6 +18,22 @@ export const Integrations = ({
   )
   return (
     <div className={styles.integrations}>
+      <div
+        key='members'
+        className={
+          matchTab?.params.tab === 'members'
+            ? `${styles.integration} ${styles.selected}`
+            : styles.integration
+        }
+        onClick={() => {
+          if (community) history.push(`/communities/${community.id}/members`)
+        }}
+      >
+        <h4>
+          <FontAwesomeIcon icon={faAddressBook} fixedWidth={true} />
+          Members
+        </h4>
+      </div>
       {community?.owner_id === auth.id && (
         <div
           key='settings'
