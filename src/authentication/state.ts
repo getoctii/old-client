76906f7ce6a3f86326842cd58a858720f48f9eb1
Lleyton Gray <lleyton@innatical.com>
2020-10-07 11:56:17 -0700

    Clear token if expired

--- src/authentication/state.ts
+++ src/authentication/state.ts
@@ -1,4 +1,4 @@
-import { useState, useMemo } from 'react'
+import { useState, useMemo, useEffect } from 'react'
 import decode from 'jwt-decode'
 import { createContainer } from 'unstated-next'
 
@@ -7,11 +7,19 @@ const useAuth = () => {
     localStorage.getItem('neko-token') || null
   )
   const authenticated = !!token
-  const id = useMemo<string | null>(
-    () => (token ? (decode(token) as any).sub : null),
+  const payload = useMemo<{ sub: string; exp: number } | null>(
+    () => (token ? (decode(token) as any) : null),
     [token]
   )
-  return { token, id, authenticated, setToken }
+  // TODO: Check every second for token expiry
+  useEffect(() => {
+    if (payload?.exp && payload.exp <= Math.floor(Date.now() / 1000)) {
+      setToken(null)
+      localStorage.removeItem('neko-token')
+    }
+  }, [payload])
+
+  return { token, id: payload?.sub ?? null, authenticated, setToken }
 }
 
 export const Auth = createContainer(useAuth)
