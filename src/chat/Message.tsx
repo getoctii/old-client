92a385c7006655ff6e3eb5ea57d5e8f58c4c519a
Adam <adam@atiktech.co>
2020-10-07 21:44:09 -0500

    :art: improved our message view

--- src/chat/Message.tsx
+++ src/chat/Message.tsx
@@ -1,94 +1,131 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, memo, ReactChildren } from 'react'
 import styles from './Message.module.scss'
 import moment from 'moment'
 import ReactMarkdown from 'react-markdown'
 import { useMeasure } from 'react-use'
 
-const Message = ({
+const ImageEmbed = ({
   children,
-  avatar,
-  timestamp,
-  author,
-  primary,
-  onresize
+  onResize
 }: {
-  children: string
-  avatar: string
-  timestamp: string
-  author: string
-  primary: boolean
-  onresize: () => void
+  children: ReactChildren
+  onResize: () => void
 }) => {
   const [ref, size] = useMeasure<HTMLDivElement>()
-  useEffect(() => onresize(), [size, onresize])
+  useEffect(() => {
+    onResize()
+  }, [size, onResize])
+  return <div ref={ref}>{children}</div>
+}
 
-  return (
-    <div className={primary ? styles.primary : styles.message} ref={ref}>
-      {primary && (
-        <img
-          className={styles.avatar}
-          src={avatar}
-          alt={`${author}'s Profile`}
-        />
-      )}
-      <div className={`${styles.content} ${!primary ? styles.spacer : ''}`}>
+const Message = memo(
+  ({
+    children,
+    avatar,
+    timestamp,
+    author,
+    primary,
+    onResize
+  }: {
+    children: string
+    avatar: string
+    timestamp: string
+    author: string
+    primary: boolean
+    onResize: () => void
+  }) => {
+    return (
+      <div className={primary ? styles.primary : styles.message}>
         {primary && (
-          <h2>
-            {author}
-            <span>{moment.utc(timestamp).local().calendar()}</span>
-          </h2>
+          <img
+            className={styles.avatar}
+            src={avatar}
+            alt={`${author}'s Profile`}
+          />
         )}
-        <ReactMarkdown
-          skipHtml={false}
-          escapeHtml={true}
-          unwrapDisallowed={true}
-          allowedTypes={[
-            'root',
-            'text',
-            'paragraph',
-            'strong',
-            'emphasis',
-            'delete',
-            'link'
-          ]}
-          renderers={{
-            link: (props) => {
-              if (
-                /^https:\/\/file\.coffee\/u\/[a-zA-Z0-9_-]{7,14}\.(png|jpeg|jpg|gif)/g.test(
-                  props.href
+        <div className={`${styles.content} ${!primary ? styles.spacer : ''}`}>
+          {primary && (
+            <h2 key='username'>
+              {author}
+              <span>{moment.utc(timestamp).local().calendar()}</span>
+            </h2>
+          )}
+          <ReactMarkdown
+            skipHtml={false}
+            escapeHtml={true}
+            unwrapDisallowed={true}
+            allowedTypes={[
+              'root',
+              'text',
+              'paragraph',
+              'strong',
+              'emphasis',
+              'delete',
+              'link',
+              'heading'
+            ]}
+            renderers={{
+              heading: (props) => <p>{props.children}</p>,
+              paragraph: (props) => {
+                const content = props.children.flatMap(
+                  (child: any, index: number) =>
+                    typeof child === 'object' &&
+                    child.key &&
+                    !!child.key.match(/link/g) ? (
+                      /^https:\/\/file\.coffee\/u\/[a-zA-Z0-9_-]{7,14}\.(png|jpeg|jpg|gif)/g.test(
+                        child.props.href
+                      ) ? (
+                        [
+                          <p>{child}</p>,
+                          <div className={styles.imageEmbed}>
+                            <img
+                              alt='chat'
+                              src={
+                                child.props.href.match(
+                                  /^https:\/\/file\.coffee\/u\/[a-zA-Z0-9_-]{7,14}\.(png|jpeg|jpg|gif)$/g
+                                )?.[0]
+                              }
+                            />
+                          </div>
+                        ]
+                      ) : (
+                        <p>{child}</p>
+                      )
+                    ) : (
+                      <p>{child}</p>
+                    )
                 )
-              )
+                const paragraphs = content.filter((e: any) => e.type === 'p')
+                const images = content.filter((e: any) => e.type === 'div')
                 return (
-                  <div className={styles.imageEmbed}>
-                    <a
-                      href={props.href}
-                      target='_blank'
-                      rel='noopener noreferrer'
-                    >
-                      {props.children}
-                    </a>
-                    <img
-                      alt='chat'
-                      src={
-                        props.href.match(
-                          /^https:\/\/file\.coffee\/u\/[a-zA-Z0-9_-]{7,14}\.(png|jpeg|jpg|gif)$/g
-                        )?.[0]
-                      }
-                    />
-                  </div>
+                  <>
+                    {[
+                      <div key='text'>
+                        <p>
+                          {paragraphs.flatMap((p: any) => p.props.children)}
+                        </p>
+                      </div>,
+                      <ImageEmbed key='images' onResize={onResize}>
+                        {images.map((img: any, index: any) => (
+                          <div {...img.props} key={index} />
+                        ))}
+                      </ImageEmbed>
+                    ]}
+                  </>
                 )
-              return (
+              },
+              link: (props) => (
                 <a href={props.href} target='_blank' rel='noopener noreferrer'>
                   {props.children}
                 </a>
               )
-            }
-          }}
-          source={children}
-        />
+            }}
+            source={children}
+          />
+        </div>
       </div>
-    </div>
-  )
-}
+    )
+  }
+)
 
 export default Message
