d48a4c53e9ce6db020c3d595394a7983fec83daf
Lleyton Gray <lleyton@innatical.com>
2021-06-23 16:02:27 -0700

    image embeds

--- src/chat/embeds/File.tsx
+++ src/chat/embeds/File.tsx
@@ -10,7 +10,7 @@ import { ModalTypes } from '../../utils/constants'
 import styles from './File.module.scss'
 
 const isFile = (url: string) =>
-  /^https:\/\/innstor\.innatical\.com\/[A-Za-z0-9_-]+/g.test(url)
+  /^https:\/\/cdn\.octii\.chat\/uploads\/[A-Za-z0-9_-]+/g.test(url)
 
 const FilePreview: FC<{ url: string }> = ({ url }) => {
   return (
@@ -19,73 +19,60 @@ const FilePreview: FC<{ url: string }> = ({ url }) => {
 }
 
 const FileEmbed: FC<{ url: string }> = ({ url }: { url: string }) => {
-  const { data: image } = useQuery(
-    ['image', url],
-    async () => {
-      return (
-        await axios.get<{
-          type: string
-          mime: string
-          data?: { width: number; height: number }
-        }>(`${url}/info`)
-      ).data
-    },
-    {
-      enabled: isFile(url)
-    }
-  )
   const ui = UI.useContainer()
   const [showPlaceholder, setShowPlaceholder] = useState(true)
-  if (!image) return <></>
-  if (image.type === 'other') {
-    return (
-      <div className={styles.file}>
-        <FontAwesomeIcon icon={faFileAlt} />
-        <div className={styles.info}>
-          <small>File</small>
-          <p>{image?.mime.split('/')[1]}</p>
-        </div>
-        <Button type='button' onClick={() => window.open(url)}>
-          Open in Browser
-        </Button>
-      </div>
-    )
-  }
-  if (image.type === 'image') {
-    return (
-      <div
-        className={styles.imageEmbed}
-        onClick={() => {
-          ui.setModal({
-            name: ModalTypes.PREVIEW_IMAGE,
-            rounded: false,
-            props: { url }
-          })
-        }}
-      >
-        {showPlaceholder && (
-          <div
-            style={{
-              width: (image.data?.width ?? 600) > 600 ? 600 : image.data?.width,
-              height:
-                (image.data?.height ?? 300) > 300 ? 300 : image.data?.height
-            }}
-            className={styles.imagePlaceholder}
-          />
-        )}
-
-        <img
-          className={showPlaceholder ? styles.hide : ''}
-          onLoad={() => setShowPlaceholder(false)}
-          onError={() => setShowPlaceholder(false)}
-          width={(image.data?.width ?? 600) > 600 ? 600 : image.data?.width}
-          height={(image.data?.height ?? 300) > 300 ? 300 : image.data?.height}
-          alt={url}
-          src={url}
+  // if (image.type === 'other') {
+  //   return (
+  //     <div className={styles.file}>
+  //       <FontAwesomeIcon icon={faFileAlt} />
+  //       <div className={styles.info}>
+  //         <small>File</small>
+  //         <p>{image?.mime.split('/')[1]}</p>
+  //       </div>
+  //       <Button type='button' onClick={() => window.open(url)}>
+  //         Open in Browser
+  //       </Button>
+  //     </div>
+  //   )
+  // }
+  // if (image.type === 'image') {
+  return (
+    <div
+      className={styles.imageEmbed}
+      onClick={() => {
+        ui.setModal({
+          name: ModalTypes.PREVIEW_IMAGE,
+          rounded: false,
+          props: { url }
+        })
+      }}
+    >
+      {showPlaceholder && (
+        <div
+          // style={{
+          //   width: (image.data?.width ?? 600) > 600 ? 600 : image.data?.width,
+          //   height: (image.data?.height ?? 300) > 300 ? 300 : image.data?.height
+          // }}
+          className={styles.imagePlaceholder}
         />
-      </div>
-    )
-  }
+      )}
+
+      <img
+        className={showPlaceholder ? styles.hide : ''}
+        onLoad={() => setShowPlaceholder(false)}
+        onError={() => setShowPlaceholder(false)}
+        style={{
+          maxWidth: 600,
+          maxHeight: 300
+        }}
+        // width={(image.data?.width ?? 600) > 600 ? 600 : image.data?.width}
+        // height={(image.data?.height ?? 300) > 300 ? 300 : image.data?.height}
+        alt={url}
+        src={url}
+      />
+    </div>
+  )
+  // }
 
   return <div></div>
 }
